68429233cf29b22e50f1bd07e1e79e9daf9130a4
Shubham Pathak <95904081+Shubhh4@users.noreply.github.com>
2023-06-11 23:45:26 +0530

    Update TokenMaster.sol

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -16,7 +16,7 @@ contract TokenMaster is ERC721 {
         uint256 maxTickets;
         string date;
         string time;
-        string location;
+        string locations;
     }
 
     mapping(uint256 => Occasion) occasions;
